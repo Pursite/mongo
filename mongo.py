b7c1b822ff0d3a20d17c462bd5005bce31c48149
Pursite <armin10gp@gmail.com>
2025-05-17 16:24:51 +0330

    edit(secound-Train)

--- mongo.py
+++ mongo.py
@@ -1,28 +1,29 @@
-
+import scrapy
 from pymongo import MongoClient
 
-# اتصال به MongoDB (در اینجا به سرور محلی)
-client = MongoClient("mongodb://localhost:27017/")
 
-# ساخت/اتصال به دیتابیس و کالکشن
-db = client["mydatabase"]
-collection = db["users"]
+class BooksSpider(scrapy.Spider):
+    name = "books"
 
-# ---------- CREATE ----------
-new_user = {"name": "Ali", "age": 25, "email": "ali@example.com"}
-insert_result = collection.insert_one(new_user)
-print("Inserted ID:", insert_result.inserted_id)
+    start_urls = ['http://books.toscrape.com']
 
-# ---------- READ ----------
-print("\nAll users:")
-for user in collection.find():
-    print(user)
+    def __init__(self):
+        # اتصال به MongoDB
+        self.client = MongoClient("mongodb://localhost:27017/")
+        self.db = self.client["bookdb"]
+        self.collection = self.db["books"]
 
-# ---------- UPDATE ----------
-collection.update_one(
-    {"name": "Ali"},
-    {"$set": {"age": 26}}
-)
+    def parse(self, response):
+        for book in response.css('article.product_pod'):
+            item = {
+                'title': book.css('h3 a::attr(title)').get(),
+                'price': book.css('p.price_color::text').get()
+            }
+            # ذخیره در MongoDB
+            self.collection.insert_one(item)
+            yield item
 
-# ---------- DELETE ----------
-collection.delete_one({"name": "Ali"})
+        # دنبال کردن صفحه بعد
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
